090424b2952f1e6c960b40ea1d339d3e1d160416
akkerise <akkerise@gmail.com>
2025-03-04 19:17:23 +0000

    :tada: refactor

--- config.lua
+++ config.lua
@@ -6,8 +6,20 @@ lvim.colorscheme = init.colorscheme
 lvim.log.level = init.log.level
 lvim.reload_config_on_save = init.reload_config_on_save
 
+
+
+vim.opt.cmdheight = 2 -- more space in the neovim command line for displaying messages
+vim.opt.guifont = "monospace:h17" -- the font used in graphical neovim applications
+vim.opt.shiftwidth = 2 -- the number of spaces inserted for each indentation
+vim.opt.tabstop = 2 -- insert 2 spaces for a tab
+vim.opt.relativenumber = true -- relative line numbers
+vim.opt.wrap = true -- wrap lines
 vim.opt.incsearch = true
-vim.opt.shiftwidth = 2
+
+-- use treesitter folding
+vim.opt.foldmethod = "expr"
+vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
+
 
 
 lvim.builtin.telescope.active = true
